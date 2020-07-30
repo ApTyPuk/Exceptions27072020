c2fb02f339d4b5f83a68e76badb1f64baa608a3c
PrIvATe <kpackalv@yahoo.com>
2020-07-30 21:34:38 +0300

    new Example of try and catch 30/07/20

--- src/com/Artur/Example.java
+++ src/com/Artur/Example.java
@@ -7,27 +7,30 @@ import java.util.Scanner;
 public class Example {
 
     public static void main(String[] args) {
-        int result = divide();
-        System.out.println(result);
+        try {
+            int result = divide();
+            System.out.println(result);
+        } catch(ArithmeticException | NoSuchElementException e) {
+            System.out.println(e.toString());
+            System.out.println("Unable to perform division, autopilot shutting down");
+        }
     }
 
     private static int divide(){
         int x, y;
-        try{
-            x = getInt();
-            y = getInt();
-        }catch (NoSuchElementException e){
-            throw new ArithmeticException("no suitable input");
-        }
-        System.out.println("x is " + x+ ", y is " + y);
-        try{
-            return x / y;
-        }catch  (ArithmeticException e){
-            throw new ArithmeticException("attempt to divide by zero");
-        }
+//        try {
+        x = getInt();
+        y = getInt();
+        System.out.println("x is " + x + ", y is " + y);
+        return x / y;
+//        } catch(NoSuchElementException e) {
+//            throw new ArithmeticException("no suitable input");
+//        } catch(ArithmeticException e) {
+//            throw new ArithmeticException("attempt to divide by zero");
+//        }
     }
 
-    private  static int getInt(){
+    private static int getInt(){
         Scanner s = new Scanner(System.in);
         System.out.println("Please enter an integer ");
         while(true){
